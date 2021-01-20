c5cc439f04ef32bcbed0d0ceba91517e959ebd22
Daniel <daniru9@gmail.com>
2021-01-20 21:52:11 +0100

    componente inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NosotrosComponent } from './components/nosotros/nosotros.component';
 import { ContactoComponent } from './components/contacto/contacto.component';
+import { InicioComponent } from './components/inicio/inicio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NosotrosComponent,
-    ContactoComponent
+    ContactoComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
